1fe31c2f6ae13e74771546875b337e66d0e6ceb2
shuto3726 <sss3726mmm@gmail.com>
2021-08-21 05:11:08 +0000

    finish

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,27 +4,36 @@ class TodolistsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
   end
-  
+
   def index
-    @lists = List.all  
+    @lists = List.all
   end
-  
+
   def show
-    @list = List.find(params[:id])  
+    @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to todolist_path(list.id)  
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
 
 
